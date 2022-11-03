30104eead86756d04f8e10636c78c7456096209f
theocita <theocitaallen@gmail.com>
2022-11-03 17:41:20 -0400

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const form =  document.querySelector("#create-task-form")
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", submitForm)
 });
+
+function submitForm(e) {
+  e.preventDefault()
+
+  const todoList = document.querySelector("#new-task-description").value
+  const li = document.createElement("li")
+  li.textContent = todoList
+
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = "X"
+  
+  deleteButton.addEventListener("click", () => li.remove)
+  li.append(deleteButton)
+  tasks.appendChild(li)
+  form.reset()
+}
+
